689fdab20a83d70a315591749bb1d4b5a4fe0c4b
boseong <boseongkim@autocrypt.io>
2024-12-20 12:36:40 +0900

    Initial Commit

---
+++ VonaTaskOrganizer/VonaTaskOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VonaTaskOrganizerApp.swift
+//  VonaTaskOrganizer
+//
+//  Created by Boseong Kim on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct VonaTaskOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VonaTaskOrganizerTests/VonaTaskOrganizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  VonaTaskOrganizerTests.swift
+//  VonaTaskOrganizerTests
+//
+//  Created by Boseong Kim on 12/20/24.
+//
+
+import Testing
+@testable import VonaTaskOrganizer
+
+struct VonaTaskOrganizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
